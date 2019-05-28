be17f054db30e6a7e4a5a90508e6ef923537087a
叶志斌 <yzbzhouning@163.com>
2019-05-29 07:33:47 +0800

    no message

--- think_python_price/c13.py
+++ think_python_price/c13.py
@@ -56,12 +56,73 @@
 # print(middle([1,2,3,4,5]))
 
     
-def middle(ele):
-    res=ele[1:len(ele)-1]
-    print(res)
-    return
+# def middle(ele):
+#     res=ele[1:len(ele)-1]
+#     print(res)
+#     return
 
-print(middle([1,2,3,4,5]))
+# print(middle([1,2,3,4,5]))
 
 
+# def is_sorted(ele):
+#     if sorted(ele)==ele:
+#         return True
+#     else:
+#         return False
+# # print(is_sorted([1,2,2])
+# # #print(is_sorted(['b','a'])
+# print(is_sorted([1,2,2,]))
+# print(is_sorted(['b','a']))
+
+# def is_sorted(ele):
+#     for i in range(len(ele)-1):
+#         if ele[i] > ele[i+1]:
+#             return False
+#     return True
+
+# print(is_sorted([1,2,2,]))
+# print(is_sorted(['b','a']))
+
+# def is_anagram(s, t):
+#     s = s.lower().replace(" ", "")
+#     t = t.lower().replace(" ", "")
     
+#     first = list(s)
+#     second = list(t)
+
+#     first.sort()
+#     second.sort()
+    
+#     return first == second
+
+# print(is_anagram("Mary", "Army"))
+# print(is_anagram("Jim Morrison", "Mr Mojo Risin"))
+# print(is_anagram("War", "Peace"))
+
+# def is_anagram(listik1, listik2):
+#     if len(listik1) == len(listik2):
+#         listik1 = list(listik1)
+#         listik2 = list(listik2)
+#         listik1.sort()
+#         listik2.sort()
+#         return listik1 == listik2
+#     else:
+#         return False
+    
+# print(is_anagram('orchestra','carthorse'))
+# print(is_anagram('sport','porta'))
+# print(is_anagram(' ',' ')) # is it ok or not?!
+
+
+def is_anagram(ele,ele2):
+    if len(ele)==len(ele2):
+        ele=list(ele)
+        ele2=list(ele2)
+        ele.sort()
+        ele2.sort()
+        return ele==ele2
+    else:
+        return False
+print(is_anagram('orchestra tvnm','carthorse mvtn'))
+print(is_anagram('sport','porta'))
+print(is_anagram(' ',' ')) # is it ok or not?!
