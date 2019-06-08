303a88f37c6aa6e362e9a6b4178cc960a77e8483
叶志斌 <yzbzhouning@163.com>
2019-06-08 16:04:03 +0800

    no message

--- think_python_price/c002.py
+++ think_python_price/c002.py
@@ -1,7 +1,13 @@
-PI=3.1415926
+# PI=3.1415926
 
-def f(X):
-    return X*2
+# def f(X):
+#     return X*2
 
-def g(a,b):
-    return a+b
+# def g(a,b):
+#     return a+b
+list_of_lists=['foo','abc','def']
+everything=[]
+for chunk in list_of_lists:
+    everything.extend(chunk)
+
+print(everything)
