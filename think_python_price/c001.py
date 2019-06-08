ff9e76f746a977f949bef31e0a4a50cfcb5d2ea9
叶志斌 <yzbzhouning@163.com>
2019-06-08 15:24:16 +0800

    no message

--- think_python_price/c001.py
+++ think_python_price/c001.py
@@ -8,6 +8,53 @@ import numpy as np
 # data={i:randn() for i in range(7)}
 # print(data)
 
-import matplotlib.pyplot as plt
-plt.plot(np.random.randn(50).cumsum())
-#plt.show()
+# import matplotlib.pyplot as plt
+# plt.plot(np.random.randn(50).cumsum())
+# #plt.show()
+
+# import c002
+# result=c002.f(5)
+# pi=c002.PI
+
+
+# print(result)
+# print(pi)
+
+
+# from c002 import f,g,PI
+# result=g(5,PI)
+
+# print(result)
+
+
+# import c002 as sm
+# from c002 import PI as pi ,g as gf
+# r1=sm.f(pi)
+# r2=gf(6,pi)
+
+# print(r2)
+
+
+# def add_and(a,b,c=None):
+#     result=a+b
+#     if c is not None:
+#         result=result*c
+#     return result
+
+# print(add_and(1,2,2))
+
+
+# sequence=[1,2,None,4,None,5]
+# total=0
+# for value in sequence:
+#     if value is None:
+#         continue
+#     total +=value
+
+# print(total)
+
+# for i in range(4):
+#     for j in range(4):
+#         if j>i:
+#             break
+#         print(i,j)
