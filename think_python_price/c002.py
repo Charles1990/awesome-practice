1e3aa4b835763854cba9a5528f4ed7ab569a7f79
叶志斌 <yzbzhouning@163.com>
2019-06-09 16:09:45 +0800

    no message

--- think_python_price/c002.py
+++ think_python_price/c002.py
@@ -5,9 +5,21 @@
 
 # def g(a,b):
 #     return a+b
-list_of_lists=['foo','abc','def']
-everything=[]
-for chunk in list_of_lists:
-    everything.extend(chunk)
+# list_of_lists=['foo','abc','def']
+# everything=[]
+# for chunk in list_of_lists:
+#     everything.extend(chunk)
 
-print(everything)
+# print(everything)
+
+words=['apple','bat','bar','atom','book','cup']
+by_letter={}
+
+for word in words:
+    letter=word[0]
+    if letter not in by_letter:
+        by_letter[letter]=[word]
+    else:
+        by_letter[letter].append(word)
+
+print(by_letter)
