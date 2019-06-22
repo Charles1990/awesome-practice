69e0339db500341314e3c5fcef731b0e04b69c10
叶志斌 <yzbzhouning@163.com>
2019-06-22 09:51:16 +0800

    no message

--- think_python_price/c002.py
+++ think_python_price/c002.py
@@ -12,14 +12,96 @@
 
 # print(everything)
 
-words=['apple','bat','bar','atom','book','cup']
-by_letter={}
+# by_letter={}
 
-for word in words:
-    letter=word[0]
-    if letter not in by_letter:
-        by_letter[letter]=[word]
-    else:
-        by_letter[letter].append(word)
+# for word in words:
+#     letter=word[0]
+#     if letter not in by_letter:
+#         by_letter[letter]=[word]
+#     else:
+#         by_letter[letter].append(word)
 
-print(by_letter)
+# print(by_letter)
+
+
+# words=['apple','bat','bar','atom','book','cup','a']
+
+# lt=[]
+
+# for i in words:
+#     if len(i)>2:
+#         lt.append(i)
+
+# print(lt)
+
+# all_data = [['John', 'Emily', 'Michael', 'Mary', 'Steven'],['Maria', 'Juan', 'Javier', 'Natalia', 'Pilar']]
+# lt=[]
+
+# for names in all_data:
+    
+#     for name in names:
+#         if names.count('e')>=2:
+#             lt1.extend(names)
+    
+
+
+    # enough_es=[name for name in names if name.count('e')>=2]
+    # lt.extend(enough_es)
+
+# print(lt)
+
+
+# string=['a','as','bata','car','dove','python']
+# lt=[]
+
+# for x in string:
+#     if len(x)>2:
+#         x=x.upper()
+#         lt.append(x)
+
+# # print(lt)
+# all_data = [['John', 'Emily', 'Michael', 'Mary', 'Steven'],['Maria', 'Juan', 'Javier', 'Natalia', 'Pilar']]
+# lt=[]
+
+# for names in all_data:
+#     x=[name for name in names if name.count('e')>=2]
+#     lt.extend(x)
+
+# print(lt)
+
+# for names in all_data:
+#     for name in names:
+#         if name.count('e')>=2:
+#             lt.append(name)
+# print(lt)
+
+# a=[]
+# def func():
+    
+#     for i in range(5):
+#         a.append(i)
+
+# print(a)
+
+# def f():
+#     a=5
+#     b=6
+#     c=7
+#     return a,b,c
+# a,b,c=f()
+# print(f())
+
+states = ['   Alabama ', 'Georgia!', 'Georgia', 'georgia', 'FlOrIda',
+  'south   carolina##', 'West virginia?']
+
+import re
+def clean_string(strings):
+    result=[]
+    for value in strings:
+        value=value.strip()
+        value=re.sub('[!#?]','',value)
+        value=value.title()
+        result.append(value)
+    return result
+
+print(clean_string(states))
